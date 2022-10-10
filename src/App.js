d380f63a2de5becdf8f8473be63f0602edcbdde0
Anusuya20 <anusuyakumarasamy01@gmail.com>
2022-10-10 11:21:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Js Exercises</h1>
+        <div class="navbar">
+        <a class="links" href="https://anusuya20.github.io/scrolldown_react">Ex 1: Simple page with Image, Button and Scrollview</a><br/>
+        <a class="links" href="https://anusuya20.github.io/cardview_react/">Ex 2: Create simple card view with CSS styling</a><br/>
+        <a class="links" href="https://anusuya20.github.io/currentdate-time_react/">Ex 3: Displaying Current date with running Time</a><br/>
+        <a class="links" href="https://anusuya20.github.io/props_react/">Ex 4.Display static data using props</a><br/>
+        <a class="links" href=" https://anusuya20.github.io/toggle_react/">Ex 5: Display content using toggle button</a><br/>
+        <a class="links" href="https://anusuya20.github.io/calculator_react/">Ex.6 Simple Calculator</a><br/>
+        <a class="links" href=" https://anusuya20.github.io/formvalidator_react/">Ex 7: Simple form validator</a><br/>
+        <a class="links" href="https://anusuya20.github.io/reusablecomponent_react/">Ex 8.Reusable Component</a><br/>
+        <a class="links" href="https://anusuya20.github.io/todo_react/">Ex 9:Todo list</a><br/>
+        <a class="links" href="https://anusuya20.github.io/restapi_react/">Ex 10. Simple application using Service Api </a><br/>
+      </div>
       </header>
     </div>
   );
